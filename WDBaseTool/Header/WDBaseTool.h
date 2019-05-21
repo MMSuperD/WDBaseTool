7440321f453408bdd0f42af96b71758188db3eb8
GodSuperD <1653591454@qq.com>
2019-05-21 08:56:22 +0800

    2.添加了头文件

---
+++ WDBaseTool/Header/WDBaseTool.h
@@ -0,0 +1,14 @@
+//
+//  WDBaseTool.h
+//  GY
+//
+//  Created by wangwei on 2019/5/21.
+//  Copyright © 2019 SuperD. All rights reserved.
+//
+
+#ifndef WDBaseTool_h
+#define WDBaseTool_h
+
+#import "WD_SelectCardView.h"
+
+#endif /* WDBaseTool_h */
